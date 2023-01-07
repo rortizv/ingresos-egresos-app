c00421dc1cab95b1dfbb147646538cfc767ed5de
Rafael Ortiz <rortiz@inkremental.co>
2023-01-07 12:41:53 -0500

    Validaciones formulario habilita botón Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegistroComponent } from './auth/registro/registro.component';
@@ -28,7 +30,8 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/registro/registro.component.ts
+++ src/app/auth/registro/registro.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
+
+  public registroForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email] ],
+      password: ['', Validators.required]
+    });
+  }
+
+  crearUsuario() {
+    console.log(this.registroForm.value);
+  }
 
 }
