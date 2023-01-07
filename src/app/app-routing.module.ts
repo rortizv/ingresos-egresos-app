3acd698c69aa1aa51afb3b1b40799ceef905fd5a
Rafael Ortiz <rortiz@inkremental.co>
2023-01-07 12:07:16 -0500

    Agregar html componentes ingres-egreso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegistroComponent } from './auth/registro/registro.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { dashboardRoutes } from './dashboard/dashboard.routes';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
-  { path: '', component: DashboardComponent },
+  { path: '', component: DashboardComponent, children: dashboardRoutes },
   { path: '**', redirectTo: '' }
 ];
 
