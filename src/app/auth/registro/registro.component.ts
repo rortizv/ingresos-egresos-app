32e87c70947a2e1001ea6e07ec1f0d83087c4646
Rafael Ortiz <rortiz@inkremental.co>
2023-01-08 10:03:50 -0500

    Crear usuario

--- src/app/auth/registro/registro.component.ts
+++ src/app/auth/registro/registro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-registro',
@@ -10,7 +12,9 @@ export class RegistroComponent implements OnInit {
 
   public registroForm: FormGroup = new FormGroup({});
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.registroForm = this.fb.group({
@@ -21,7 +25,16 @@ export class RegistroComponent implements OnInit {
   }
 
   crearUsuario() {
-    console.log(this.registroForm.value);
+    if (this.registroForm.invalid) { return };
+    const { nombre, email, password } = this.registroForm.value;
+    this.authService.crearUsuario(nombre, email, password)
+      .then(credenciales => {
+        console.log(credenciales);
+        this.router.navigate(['/']);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
 }
