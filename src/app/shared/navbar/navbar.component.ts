55c12eb0413677eac3b153b1415cb041e1722f97
Rafael Ortiz <rortiz@inkremental.co>
2023-01-15 13:35:41 -0500

    Nombre en navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription, filter } from 'rxjs';
+import { AppState } from 'src/app/app.reducer';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  
+  public nombre: string = '';
+  userSubscription: Subscription = new Subscription();
+
+  constructor(private store: Store<AppState>) { }
+
+  ngOnInit() {
+    this.userSubscription = this.store.select('user')
+      .pipe(
+        filter(({user}) => user != null)
+      )
+      .subscribe(({ user }) => this.nombre = user.nombre);
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
+  }
 
 }
